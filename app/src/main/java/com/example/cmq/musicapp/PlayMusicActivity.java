fe34106398abf6dc76432aaa71a4fc613406e763
PHÁT NGUYỄN CƯỜNG <15520601@ms.uit.edu.vn>
2017-11-09 16:03:02 +0700

    Play Local Song

---
+++ app/src/main/java/com/example/cmq/musicapp/PlayMusicActivity.java
@@ -0,0 +1,201 @@
+package com.example.cmq.musicapp;
+
+import android.annotation.SuppressLint;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class PlayMusicActivity extends AppCompatActivity {
+
+    TextView txtTitle, txtTimeProcess, txtTimeTotal;
+    SeekBar sbProcess;
+    ImageButton btnPrev, btnPlay, btnNext, btnStop;
+    ImageView imgDics;
+
+    ArrayList<Song> arraySong;
+    int indexSong=0;
+    MediaPlayer mediaPlayer;
+
+    Animation anim_dics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_music);
+
+        InitComp();
+        AddSongs();
+
+        createMediaPlayer();
+        anim_dics = AnimationUtils.loadAnimation(this, R.anim.dics_rotate);
+
+        //Play Pause Button Event
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    btnPlay.setImageResource(R.drawable.play_48);
+                    imgDics.clearAnimation();
+                } else {
+                    mediaPlayer.start();
+                    btnPlay.setImageResource(R.drawable.pause_48);
+                    imgDics.startAnimation(anim_dics);
+                }
+            }
+        });
+
+        //Stop Button Event
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                btnPlay.setImageResource(R.drawable.play_48);
+                imgDics.clearAnimation();
+                createMediaPlayer();
+            }
+        });
+
+        //Next Button Event
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                indexSong++;
+                if (indexSong > arraySong.size() - 1) {
+                    indexSong = 0;
+                }
+
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();;
+                    mediaPlayer.release();
+                }
+
+                createMediaPlayer();
+                mediaPlayer.start();
+                btnPlay.setImageResource(R.drawable.pause_48);
+                imgDics.startAnimation(anim_dics);
+            }
+        });
+
+        //Previous Button Event
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                indexSong--;
+                if (indexSong < 0) {
+                    indexSong = arraySong.size() - 1;
+                }
+
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                }
+
+                createMediaPlayer();
+                mediaPlayer.start();
+                btnPlay.setImageResource(R.drawable.pause_48);
+                imgDics.startAnimation(anim_dics);
+            }
+        });
+
+        //SeekBar Process Event
+        sbProcess.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(sbProcess.getProgress());
+            }
+        });
+    }
+
+    private void updateTime(){
+        //Update Time Process and SeekBar Process
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                @SuppressLint("SimpleDateFormat") SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+                txtTimeProcess.setText(timeFormat.format(mediaPlayer.getCurrentPosition()));
+                sbProcess.setProgress(mediaPlayer.getCurrentPosition());
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        indexSong++;
+                        if (indexSong > arraySong.size() - 1) {
+                            indexSong = 0;
+                        }
+
+                        if (mediaPlayer.isPlaying()){
+                            mediaPlayer.stop();;
+                            mediaPlayer.release();
+                        }
+
+                        createMediaPlayer();
+                        mediaPlayer.start();
+                        btnPlay.setImageResource(R.drawable.pause_48);
+                    }
+                });
+                handler.postDelayed(this, 500);
+            }
+        }, 200);
+    }
+
+    private void setTime(){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+        txtTimeTotal.setText(timeFormat.format(mediaPlayer.getDuration()));
+        sbProcess.setMax(mediaPlayer.getDuration());
+    }
+
+    private void createMediaPlayer(){
+        mediaPlayer = MediaPlayer.create(PlayMusicActivity.this, arraySong.get(indexSong).getFile());
+        txtTitle.setText(arraySong.get(indexSong).getTitle());
+        setTime();
+        updateTime();
+    }
+
+    private void AddSongs() {
+        arraySong = new ArrayList<>();
+        arraySong.add(new Song("Em gái mưa", R.raw.em_gai_mua));
+        arraySong.add(new Song("Chạm khẽ tim anh một chút thôi", R.raw.cham_khe_tim_anh_mot_chut_thoi));
+
+    }
+
+    private void InitComp() {
+        txtTitle = (TextView) findViewById(R.id.txt_TitleSong);
+        txtTimeProcess = (TextView) findViewById(R.id.txt_TimeProcess);
+        txtTimeTotal = (TextView) findViewById(R.id.txt_TimeTotal);
+
+        sbProcess = (SeekBar) findViewById(R.id.sb_Process);
+
+        btnPrev = (ImageButton) findViewById(R.id.btn_prev);
+        btnPlay = (ImageButton) findViewById(R.id.btn_play);
+        btnStop = (ImageButton) findViewById(R.id.btn_stop);
+        btnNext = (ImageButton) findViewById(R.id.btn_next);
+
+        imgDics = (ImageView) findViewById(R.id.img_Dics);
+
+    }
+}
