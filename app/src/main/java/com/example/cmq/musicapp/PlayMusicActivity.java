82635fb2e398d45f8bb9076602d33962fd5553f3
Huỳnh Trương Nhựt <luminous997@gmail.com>
2017-11-19 11:00:35 +0700

    commit

--- app/src/main/java/com/example/cmq/musicapp/PlayMusicActivity.java
+++ app/src/main/java/com/example/cmq/musicapp/PlayMusicActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.nfc.Tag;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +22,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -30,19 +34,33 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.android.gms.drive.DriveResource;
 import com.google.android.gms.drive.DriveResourceClient;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.drive.Drive;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 //import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.Drive.Builder;
 import com.google.android.gms.common.AccountPicker;
+import com.google.api.services.drive.model.File;
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -58,7 +76,7 @@ public class PlayMusicActivity extends AppCompatActivity {
     com.google.android.gms.common.SignInButton btnSignIn;
     GoogleSignInOptions gso;
     GoogleSignInClient mGoogleSignInClient;
-
+    GoogleSignInAccount mAccountData;
     ArrayList<Song> arraySong;
     int indexSong=0;
     MediaPlayer mediaPlayer;
@@ -68,6 +86,8 @@ public class PlayMusicActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String TAG =  "SIGN IN: ";
+        Log.i(TAG, "Sign in success");
         setContentView(R.layout.activity_play_music);
 
         InitComp();
@@ -78,8 +98,13 @@ public class PlayMusicActivity extends AppCompatActivity {
 
         //--------------------------------------------------//
         //RIGHT HERE
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-
+        String serverClientId = getString(R.string.sever_client_id);
+        //gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestScopes(Drive.SCOPE_FILE).build();
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(serverClientId)
+                .requestEmail()
+                .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button_gg);
@@ -90,6 +115,8 @@ public class PlayMusicActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 signIn();
+                //Intent Login = buildGoogleSignInClient().getSignInIntent();
+                //startActivityForResult(Login, RC_SIGN_IN);;
             }
         });
 //        signInButton.setOnClickListener(new View.OnClickListener() {
@@ -228,9 +255,18 @@ public class PlayMusicActivity extends AppCompatActivity {
 //        });
 
     }
+//    private GoogleSignInClient buildGoogleSignInClient() {
+//        GoogleSignInOptions signInOptions =
+//                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                        .requestScopes(Drive.SCOPE_FILE)
+//                        .build();
+//        return GoogleSignIn.getClient(this, signInOptions);
+//    }
     private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        final Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+//        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(signInIntent);
+//        updateViewWithGoogleSignInAccountTask(task);
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -238,35 +274,100 @@ public class PlayMusicActivity extends AppCompatActivity {
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
+            String TAG = "OnActivity ";
+            Log.i(TAG, "Result");
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String authCode = account.getServerAuthCode();
+
+                // Show signed-un UI
+                //updateUI(account);
+                // TODO: send code to server and exchange for access/refresh/ID tokens
+            } catch (ApiException e) {
+                Log.w(TAG, "Sign-in failed", e);
+                //updateUI(null);
+            }
+            //handleSignInResult(task);
         }
     }
+    //Ham bi sai nhung dung bo
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            String idToken = account.getIdToken();
+            //updateUI(account);
             // Signed in successfully, show authenticated UI.
             //updateUI(account);
 //             Build a drive client.
-           DriveClient mDriveClient = Drive.getDriveClient(getApplicationContext(), account);
-            // Build a drive resource client.
-           DriveResourceClient mDriveResourceClient =
-                    Drive.getDriveResourceClient(getApplicationContext(), account);
-            GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(this, Collections.singleton(DriveScopes.DRIVE));
-            credential.setSelectedAccountName(account.getDisplayName());
-            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-            com.google.api.services.drive.Drive mService = null;
-             mService = new com.google.api.services.drive.Drive.Builder(AndroidHttp.newCompatibleTransport(),  jsonFactory, credential).build();
-
+//           DriveClient mDriveClient = Drive.getDriveClient(getApplicationContext(), account);
+//            // Build a drive resource client.
+//           DriveResourceClient mDriveResourceClient = Drive.getDriveResourceClient(getApplicationContext(), account);
+//            GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(this, Collections.singleton(DriveScopes.DRIVE));
+//            credential.setSelectedAccountName(account.getDisplayName());
+//            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+//            com.google.api.services.drive.Drive mService = null;
+//            mService = new com.google.api.services.drive.Drive.Builder(AndroidHttp.newCompatibleTransport(),  jsonFactory, credential).build();
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
             //updateUI(null);
         }
+
+    }
+    private void updateViewWithGoogleSignInAccountTask(Task<GoogleSignInAccount> task) {
+        String TAG = "UPDATEVIEW";
+        Log.i(TAG, "Update view with sign in account task");
+        task.addOnSuccessListener(
+                new OnSuccessListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                        String TAG =  "SIGN IN: ";
+                        Log.i(TAG, "Sign in success");
+                        // Build a drive client.
+                        DriveClient mDriveClient = Drive.getDriveClient(getApplicationContext(), googleSignInAccount);
+                        // Build a drive resource client.
+                        final DriveResourceClient mDriveResourceClient =
+                                Drive.getDriveResourceClient(getApplicationContext(), googleSignInAccount);
+                        Query query = new Query.Builder()
+                                .addFilter(Filters.eq(SearchableField.TITLE, "Faded.mp3"))
+                                .build();
+                        Task<MetadataBuffer> queryTask = mDriveResourceClient.query(query);
+                        queryTask
+                                .addOnSuccessListener(new OnSuccessListener<MetadataBuffer>() {
+                                            @Override
+                                            public void onSuccess(MetadataBuffer metadataBuffer) {
+                                                // Handle results...
+                                                Metadata metadata = metadataBuffer.get(0);
+                                                String FileID = metadata.getOriginalFilename();
+                                                txtTitle.setText(FileID);
+                                            }
+                                        })
+                                .addOnFailureListener( new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Handle failure...
+                                    }
+                                });
+
+//                        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(getApplicationContext(), Collections.singleton(DriveScopes.DRIVE));
+//                        credential.setSelectedAccountName(credential.getSelectedAccountName());
+//                        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+//                        com.google.api.services.drive.Drive mService = null;
+//                        mService = new com.google.api.services.drive.Drive.Builder(AndroidHttp.newCompatibleTransport(),  jsonFactory, credential).build();
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                String TAG = "SIGG IN: ";
+                                Log.w(TAG, "Sign in failed", e);
+                            }
+                        });
     }
     @Override
     protected void onStart()
